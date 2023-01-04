4de50177583682bbfcc414a67ca7e1faacf1cf95
victini <ivansaulromano100@gmail.com>
2023-01-04 07:00:49 -0300

    first commit

---
+++ nuxt.config.ts
@@ -0,0 +1,8 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+
+export default ({
+  modules: [
+    'nuxt-windicss', 'nuxt-icon'
+  ],
+  srcDir: 'src',
+})
---
+++ src/app.vue
@@ -0,0 +1,6 @@
+<template>
+  <div>
+    <h2 class="bg-red-300">text</h2>
+
+  </div>
+</template>
