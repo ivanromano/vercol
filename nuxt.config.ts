7815c71f1b92c52da1d11a5b05b648238481d735
victini <ivansaulromano100@gmail.com>
2023-01-04 07:08:40 -0300

    vercel dale la concha de tu vieja

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,8 +1,22 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
+import Components from 'unplugin-vue-components/vite';
+import { AntDesignVueResolver } from 'unplugin-vue-components/resolvers';
 
 export default ({
   modules: [
     'nuxt-windicss', 'nuxt-icon'
   ],
   srcDir: 'src',
+  vite: {
+    plugins: [
+      Components({
+        // add option {resolveIcons: true} as parameter for resolving problem with icons
+        resolvers: [AntDesignVueResolver({resolveIcons: true})],
+      }),
+    ],
+  ssr: {
+    noExternal: ['moment', 'compute-scroll-into-view', 'ant-design-vue', 'vue3-carousel', '@ant-design/icons-vue'],
+    },
+  
+  },
 })
